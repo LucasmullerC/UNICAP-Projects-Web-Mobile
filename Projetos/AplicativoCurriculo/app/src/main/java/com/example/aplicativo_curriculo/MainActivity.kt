9d4d75a56f01d9f42f6c34c74844073f6c7d2d4f
Lucas Müller <lucas.mullerqdv@gmail.com>
2021-12-04 11:11:54 -0300

    Aplicativo-Curriculo

---
+++ Projetos/AplicativoCurriculo/app/src/main/java/com/example/aplicativo_curriculo/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.aplicativo_curriculo
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.aplicativo_curriculo.databinding.ActivityMainBinding
+import android.content.Intent
+import android.net.Uri
+import android.text.Html
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            val mailId = "lucas.mullerqdv@gmail.com"
+            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", mailId, null))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject text here")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml("<p><b>Your message here</b></p>"))
+            startActivity(Intent.createChooser(emailIntent, "Send email..."))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
